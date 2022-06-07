9e89a2d07ea5ef94bbd4faff5f5d7b369e9b7a8c
Emma <ethana290@gmail.com>
2022-06-07 08:35:15 -0600

    hi

---
+++ river/target/debug/deps/river-0f8c4dd0b9a7b04f.d
@@ -0,0 +1,5 @@
+/home/emma/.dotfiles/river/target/debug/deps/river-0f8c4dd0b9a7b04f: src/main.rs
+
+/home/emma/.dotfiles/river/target/debug/deps/river-0f8c4dd0b9a7b04f.d: src/main.rs
+
+src/main.rs:
---
+++ river/target/debug/river.d
@@ -0,0 +1,1 @@
+/home/emma/.dotfiles/river/target/debug/river: /home/emma/.dotfiles/river/src/main.rs
