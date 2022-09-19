9d2b9ff70d7397f4614b34e0ee35412733559550
Emma <ethana290@gmail.com>
2022-09-19 17:51:36 -0600

    a

--- deskwall.sh
+++ deskwall.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sww img -o DP-2 $(find /home/emma/Pictures/Wallpapers -type f | shuf -n 1)
-sww img -o HDMI-A-1 $(find /home/emma/Pictures/Wallpapers -type f | shuf -n 1)
-sww img -o HDMI-A-2 $(find /home/emma/Pictures/Wallpapers -type f | shuf -n 1)
+swww img -o DP-2 $(find /home/emma/Pictures/Wallpapers -type f | shuf -n 1)
+swww img -o HDMI-A-1 $(find /home/emma/Pictures/Wallpapers -type f | shuf -n 1)
+swww img -o HDMI-A-2 $(find /home/emma/Pictures/Wallpapers -type f | shuf -n 1)
