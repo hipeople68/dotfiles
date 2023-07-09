e770673ab39df141884442610a56b02864b1237c
hipeople68 <ethana290@gmail.com>
2023-07-09 03:38:11 -0600

    a

--- river_lap/keybinds.sh
+++ river_lap/keybinds.sh
@@ -61,3 +61,37 @@ riverctl map normal $mod w spawn firefox
 riverctl map normal $mod f2 spawn "/home/emma/.dotfiles/lapwall.sh"
 
 riverctl declare-mode resize
+riverctl map normal $mod+Shift r enter-mode resize
+riverctl map resize None escape enter-mode normal
+
+# Super+H and Super+L to decrease/increase the main ratio of rivertile(1)
+riverctl map resize Super H send-layout-cmd rivertile "main-ratio -0.05"
+riverctl map resize Super L send-layout-cmd rivertile "main-ratio +0.05"
+
+# Super+Shift+H and Super+Shift+L to increment/decrement the main count of rivertile(1)
+riverctl map resize Super+Shift H send-layout-cmd rivertile "main-count +1"
+riverctl map resize Super+Shift L send-layout-cmd rivertile "main-count -1"
+
+# Super+Alt+{H,J,K,L} to move views
+riverctl map resize Super+Alt H move left 100
+riverctl map resize Super+Alt J move down 100
+riverctl map resize Super+Alt K move up 100
+riverctl map resize Super+Alt L move right 100
+
+# Super+Alt+Control+{H,J,K,L} to snap views to screen edges
+riverctl map resize Super+Alt+Control H snap left
+riverctl map resize Super+Alt+Control J snap down
+riverctl map resize Super+Alt+Control K snap up
+riverctl map resize Super+Alt+Control L snap right
+
+# Super+Alt+Shift+{H,J,K,L} to resize views
+riverctl map resize Super+Alt+Shift H resize horizontal -100
+riverctl map resize Super+Alt+Shift J resize vertical 100
+riverctl map resize Super+Alt+Shift K resize vertical -100
+riverctl map resize Super+Alt+Shift L resize horizontal 100
+
+# Super + Left Mouse Button to move views
+riverctl map-pointer resize Super BTN_LEFT move-view
+
+# Super + Right Mouse Button to resize views
+riverctl map-pointer resize Super BTN_RIGHT resize-view
