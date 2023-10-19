16c24793eb76f278e936040a72f513006904fcf4
Emma <ethana290@gmail.com>
2023-10-19 10:40:25 -0300

    adding coq_nvim (maybe I should add plugins to my gitignore :neofox_think:)

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -24,23 +24,28 @@ return {
 	    require("nvim-tree").setup {}
 	  end,
 	},
+
 	{
 	    'nvim-telescope/telescope.nvim', tag = '0.1.3',
 		--or                              , branch = '0.1.x',
 	      dependencies = { 'nvim-lua/plenary.nvim' }
 	},
+
 	{
 	  'nvim-lualine/lualine.nvim',
 		dependencies = {"nvim-tree/nvim-web-devicons"}
 	},
+
 	{
 	    'windwp/nvim-autopairs',
 	    event = "InsertEnter",
 	    opts = {} -- this is equalent to setup({}) function
 	},
+
 	{
 		"catppuccin/nvim", name = "catppuccin", priority = 1000 
 	},
+
 	{
 		"nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
 		config = function () 
@@ -54,12 +59,23 @@ return {
 		})
 	end,
 	},
+
 	{
 		"neovim/nvim-lspconfig"
 	},
+
 	{
 		"ggandor/leap.nvim",
 		lazy = false,
+	},
+
+	{
+		"ms-jpq/coq_nvim",
+	},
+
+	{
+		"ms-jpq/coq.artifacts",
+		branch = artifacts,
 	}
 
 }
