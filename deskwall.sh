0cc2f38bc9600279466440416934baacf19ac8ee
Emma <ethana290@gmail.com>
2024-09-02 23:19:55 -0300

    updating desktop monitor

--- deskwall.sh
+++ deskwall.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 while true; do
-	swww img -o DP-1 $(find /home/emma/Pictures/Wallpapers -type f | shuf -n 1)
+	swww img -o DP-2 $(find /home/emma/Pictures/Wallpapers -type f | shuf -n 1)
 	sleep 1h
 done
 #swww img -o HDMI-A-1 $(find /home/emma/Pictures/Wallpapers -type f | shuf -n 1)
