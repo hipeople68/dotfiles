3ed1869b802905b6f331e32c4edeef18d34e0323
Hipeople68 <emaka04@protonmail.com>
2025-05-29 04:16:31 -0300

    set up hyprland

--- deskwall.sh
+++ deskwall.sh
@@ -4,5 +4,3 @@ while true; do
 	swww img -o DP-2 $(find /home/emma/Pictures/Wallpapers -type f | shuf -n 1)
 	sleep 1h
 done
-#swww img -o HDMI-A-1 $(find /home/emma/Pictures/Wallpapers -type f | shuf -n 1)
-#swww img -o HDMI-A-2 $(find /home/emma/Pictures/Wallpapers -type f | shuf -n 1)
