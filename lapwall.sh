6ee80d17910cfb7c3582ac1b823215eecf10d335
hipeople68 <emaka04@protonmail.com>
2025-07-22 01:40:41 -0600

    fixing wallpaper

--- lapwall.sh
+++ lapwall.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 while true; do
-	swww img -o eDP-1 $(find /home/emma/Pictures/Wallpapers -type f | shuf -n 1)
+	swww img -o eDP-2 $(find /home/emma/Pictures/Wallpapers -type f | shuf -n 1)
 	sleep 1h
 done
